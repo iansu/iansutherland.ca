654a0782a01311404b41ed05ec4fd4a1fa644eef
Ian Sutherland <ian@iansutherland.ca>
2022-08-11 17:07:11 -0600

    Update bio

--- src/components/bio.tsx
+++ src/components/bio.tsx
@@ -13,10 +13,9 @@ const Bio = (props: BioProps) => {
 
   return (
     <>
-      I&rsquo;m a full stack software developer living in Calgary, Canada. I&rsquo;m the maintainer
-      of <a href="https://github.com/facebook/create-react-app">Create React App</a> and a member of
-      the <a href="https://github.com/nodejs/node">Node.js</a> team. I also work on a number of
-      other open source projects, mostly in the JavaScript and TypeScript ecosystems.{' '}
+      I&rsquo;m a full stack software developer living in Calgary, Canada. I&rsquo;m a member of the{' '}
+      <a href="https://github.com/nodejs/node">Node.js</a> team and I also work on a number of other
+      open source projects, mostly in the JavaScript and TypeScript ecosystems.{' '}
       {more && (
         <>
           More <Link href="/about">about me</Link>.
