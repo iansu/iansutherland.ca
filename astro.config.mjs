c9d60b33137bb87d322a0d5499504e70bf7dd974
Ian Sutherland <ian@iansutherland.ca>
2022-11-23 18:02:38 -0700

    Generate sitemap and add canonical urls

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://iansutherland.ca',
+  integrations: [tailwind(), sitemap()],
 });
