b3009d8f7fc9782f725e4c4de98f908fbd407a97
Ian Sutherland <ian@iansutherland.ca>
2023-02-26 13:37:20 -0700

    Upgrade to astro v2

--- src/components/command-palette/footer.tsx
+++ src/components/command-palette/footer.tsx
@@ -1,13 +1,9 @@
+import Key from '@components/command-palette/key';
+
 export default function Footer() {
   return (
     <div className="hidden flex-wrap items-center bg-gray-50 py-2.5 px-4 text-xs text-gray-700 dark:bg-black dark:text-gray-200 sm:flex">
-      Press{' '}
-      <kbd className="mx-1 rounded border border-gray-300 bg-white px-1 dark:border-gray-700 dark:bg-gray-800">
-        Enter
-      </kbd>{' '}
-      to select,{' '}
-      <kbd className="mx-1 rounded border border-gray-300 bg-white px-1 dark:border-gray-700 dark:bg-gray-800">ESC</kbd>{' '}
-      to exit
+      Press <Key>Enter</Key> to select, <Key>Esc</Key> to exit
     </div>
   );
 }
